382416fe6fab36499da4f575481bedeea0640328
Michal Minecki <michal@mirzu.com>
2015-06-24 19:16:50 -0500

    lesson 2 end

--- plugins/restful/node/blog_posts/1.1/RestfulFourwordBlogPostsResource__1_1.class.php
+++ plugins/restful/node/blog_posts/1.1/RestfulFourwordBlogPostsResource__1_1.class.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Contains RestfulExampleArticlesResource__1_6.
+ * Contains RestfulFourwordBlogPostsResource__1_1.
  */
 
 class RestfulFourwordBlogPostsResource__1_1 extends RestfulEntityBaseNode {
